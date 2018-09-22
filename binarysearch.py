9319740e8ca5ff5fd61ad86679dead104039bbde
nikitauday <nikita.uday@gmail.com>
2018-09-22 12:01:52 +0530

    binary search

---
+++ binarysearch.py
@@ -0,0 +1,44 @@
+"""n=input("enter count")
+arr = []
+print("Enter",n,"values")
+for i in range(int(n)):
+	x = int(input())
+	arr.append(x)   """
+def count(arr,f,n):
+	l=0
+	r=n-1
+	i=first(arr,l,r,f,n)
+	if(i==-1):
+		return i
+	j=last(arr,i,n-1,f,n)
+	return j-i+1
+'''f=input("enter element to be searched")'''
+def first(arr,l,r,f,n):
+	if(l<=r):
+		m=int((l+r)/2)
+		if((int(arr[m-1])<f or m==0)and int(arr[m])==f):
+			return m
+		elif(arr[m]>f):
+			return first(arr,l,(m-1),f,n)
+		else:
+			return first(arr,(m+1),r,f,n)
+		return -1
+def last(arr,l,r,f,n):
+	if(l<=r):
+		m=int((l+r)/2)
+		if(f<(int(arr[m+1]) or m==n-1)and int(arr[m])==f):
+			return m
+		elif(arr[m]<f):
+			return last(arr,(m+1),r,f,n)
+		else:
+			return last(arr,l,(m-1),f,n)
+		return -1
+
+
+
+
+arr = [1, 2, 2, 2, 2, 3, 3, 3, 3] 
+f= 2  # Element to be counted in arr[] 
+n = len(arr) 
+c = count(arr, f, n) 
+print ("%d occurs %d times "%(f, c)) 
